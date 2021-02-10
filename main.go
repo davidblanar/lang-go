653b9776085c405d76f14fc9e63aa978d5b3504f
davidblanar <david.blanar@gmail.com>
2021-02-10 14:47:40 +0100

    args parsing, file reading

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"strings"
+	"log"
+	"io/ioutil"
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    filePath := flag.String("f", "", "Path to .l file to interpret")
+    flag.Parse()
+    if *filePath == "" {
+		log.Fatal("Missing file path")
+    }
+    
+    filePathArr := strings.Split(*filePath, ".")
+    ext := filePathArr[len(filePathArr) - 1]
+    if ext != "l" {
+    	log.Fatal(fmt.Sprintf("Invalid file type '%s'", ext))	
+    }
+
+    fileContent, err := ioutil.ReadFile(*filePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    input := string(fileContent)
+    fmt.Println(input)
 }
