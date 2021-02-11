f2c38b659d1bcf0550bc663dee26be2fc4ffd420
davidblanar <david.blanar@gmail.com>
2021-02-11 10:12:18 +0100

    tokenizer

--- main.go
+++ main.go
@@ -5,29 +5,50 @@ import (
 	"strings"
 	"log"
 	"io/ioutil"
+	"regexp"
 	"fmt"
 )
 
 func main() {
-    filePath := flag.String("f", "", "Path to .l file to interpret")
-    flag.Parse()
-    if *filePath == "" {
+	filePath := flag.String("f", "", "Path to .l file to interpret")
+	flag.Parse()
+	if *filePath == "" {
 		log.Fatal("Missing file path")
-    }
+	}
 
-    filePathArr := strings.Split(*filePath, ".")
-    ext := filePathArr[len(filePathArr) - 1]
-    if ext != "l" {
-    	log.Fatal(fmt.Sprintf("Invalid file type '%s'", ext))	
-    }
+	filePathArr := strings.Split(*filePath, ".")
+	ext := filePathArr[len(filePathArr) - 1]
+	if ext != "l" {
+		log.Fatal(fmt.Sprintf("Invalid file type '%s'", ext))	
+	}
 
-    fileContent, err := ioutil.ReadFile(*filePath)
-    if err != nil {
-        log.Fatal(err)
-    }
+	fileContent, err := ioutil.ReadFile(*filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    input := string(fileContent)
-    rs := ReadStream{input, 0}
-    fmt.Println(rs)
-    // fmt.Println(TOKEN_IDENTIFIER)
+	input := string(fileContent)
+	rs := ReadStream{input, 0}
+
+	var symbols = map[string]bool {
+		"(": true,
+		")": true,
+		"=": true,
+		"+": true,
+		"-": true,
+		"*": true,
+		"/": true,
+		"%": true,
+		">": true,
+		"<": true,
+		"[": true,
+		"]": true,
+	}
+	identifierChars := regexp.MustCompile(`[_A-Za-z]`)
+	numberChars := regexp.MustCompile(`[0-9]`)
+	whiteSpace := regexp.MustCompile(`\s`)
+	var tokens []Token
+	tokenizer := Tokenizer{rs, symbols, identifierChars, numberChars, whiteSpace, tokens}
+
+	fmt.Println(tokenizer.generate())
 }
---
+++ tokenizer.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+	"strconv"
+	"log"
+)
+
+type Token struct {
+	tokenType string
+	val interface{}
+}
+
+type Tokenizer struct {
+	rs ReadStream
+	symbols map[string]bool
+	identifierChars *regexp.Regexp
+	numberChars *regexp.Regexp
+	whiteSpace *regexp.Regexp
+	tokens []Token
+}
+
+func (t *Tokenizer) generate() []Token {
+	for {
+		if t.rs.hasNext() {
+			current := t.rs.peek()
+			if current == "#" {
+				t._skipComment()
+			} else if t._isWhiteSpace(current) {
+				t._skip()
+			} else if t._isIdentifier(current) {
+				t._readIdentifier()
+			} else if _, ok := t.symbols[current]; ok {
+			    t._readSymbol()
+			} else if t._isNumber(current) {
+				t._readNumber()
+			} else if current == "\"" {
+				t._readString()
+			} else {
+				log.Fatal("Unrecognized character %s", current)
+			}
+		} else {
+			break
+		}
+	}
+	return t.tokens
+}
+
+func (t *Tokenizer) _skipComment() {
+	for {
+		if t.rs.hasNext() && t.rs.next() != "\n" {
+			
+		} else {
+			break
+		}
+	}
+}
+
+func (t *Tokenizer) _skip() {
+	t.rs.next()
+}
+
+func (t *Tokenizer) _isWhiteSpace(char string) bool {
+	return t.whiteSpace.MatchString(char)
+}
+
+func (t *Tokenizer) _isIdentifier(char string) bool {
+	return t.identifierChars.MatchString(char)
+}
+
+func (t *Tokenizer) _isNumber(char string) bool {
+	return t.numberChars.MatchString(char)
+}
+
+func (t *Tokenizer) _readIdentifier() {
+	var sb strings.Builder
+	for {
+		if t.rs.hasNext() {
+			current := t.rs.peek()
+			if t._isWhiteSpace(current) || current == ")" {
+				break
+			}
+			sb.WriteString(t.rs.next())
+		} else {
+			break
+		}
+	}
+	token := Token{TOKEN_IDENTIFIER, sb.String()}
+	t.tokens = append(t.tokens, token)
+}
+
+func (t *Tokenizer) _readSymbol() {
+	current := t.rs.next()
+	token := Token{TOKEN_SYMBOL, current}
+	t.tokens = append(t.tokens, token)	
+}
+
+func (t *Tokenizer) _readNumber() {
+	var sb strings.Builder
+	for {
+		if t.rs.hasNext() {
+			current := t.rs.peek()
+			if current == "." || t._isNumber(current) {
+				sb.WriteString(t.rs.next())
+			} else {
+				break
+			}
+		} else {
+			break
+		}
+	}
+	val, err := strconv.ParseFloat(sb.String(), 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	token := Token{TOKEN_NUMBER, val}
+	t.tokens = append(t.tokens, token)
+}
+
+func (t *Tokenizer) _readString() {
+	// skip beginning double quote
+	t._skip()
+	var sb strings.Builder
+	for {
+		if t.rs.hasNext() {
+			current := t.rs.peek()
+			if current == "\"" {
+				// skip closing double quote
+				t._skip()
+				break
+			} else {
+				sb.WriteString(t.rs.next())
+			}
+		} else {
+			break
+		}
+	}
+	token := Token{TOKEN_STRING, sb.String()}
+	t.tokens = append(t.tokens, token)
+}
